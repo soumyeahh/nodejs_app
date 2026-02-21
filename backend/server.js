95252742921e27c634f03f97d878cbc14d1d1bbe
Soumya Polisetty <soumyapsetty@gmail.com>
2026-02-20 19:15:56 -0500

    initial project

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let items = [];
+
+app.get("/items", (req, res) => {
+    res.json(items);
+});
+
+app.post("/items", (req, res) => {
+    const item = req.body;
+    items.push(item);
+    res.json(item);
+});
+
+app.delete("/items/:id", (req, res) => {
+    items = items.filter((_, i) => i != req.params.id);
+    res.sendStatus(204);
+});
+
+app.listen(3001, () => {
+    console.log("API running on 3001");
+});
